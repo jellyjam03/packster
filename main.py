7b0cd891cc016f023d024a15f522ec80860e2075
Rotariu George <rotariugeorge0@gmail.com>
2025-01-04 01:04:49 +0200

    add help functionality

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import sys
 
+comms_to_desc = {
+    "create_archive": 'Create an archive\nUsage: python main.py create_archive [file(s)] OR directory\n',
+    "list_content": 'List content of archive\nUsage: python main.py list_content archive\n',
+    "full_unpack": 'Unpacks all the content of an archive to a specified destination\nUsage: python main.py full_unpack archive destination\n',
+    "unpack": 'Unpacks some files of the archive to a specified destination\nUsage: python main.py unpack archive destination [file(s)]\n',
+    "help": 'Gives information about command usage\nUsage: python main.py help (command)\n',
+}
+
 def create_archive(args):
+    # either all files OR single directory
     pass
 
 def list_content(args):
+    #file must be an archive
     pass
 
 def full_unpack(args):
+    #archive followed by a destination
     pass
 
 def unpack(args):
+    # archive followed by a destination and a list of files
     pass
 
 def help(args):
-    pass
+    if len(args) == 2:
+        print("Available commands:\n" + f"{' '.join(comms_to_desc.keys())}")
+        return
+
+    if args[2] not in comms_to_desc:
+        print(f"Unknown command: {args[2]}\nUse \'help\' to see available commands\n")
+        return
+    print(comms_to_desc[args[2]])
 
 def is_valid(args):
     if len(args) < 2:
@@ -37,14 +56,6 @@ comms_to_func = {
     "full_unpack": full_unpack,
     "unpack": unpack,
     "help": help,
-}
-
-comms_to_desc = {
-    "create_archive": 'Create an archive\nUsage: python main.py create_archive [file(s)] OR directory\n',
-    "list_content": 'List content of archive\nUsage: python main.py list_content archive\n',
-    "full_unpack": 'Unpacks all the content of an archive to a specified destination\nUsage: python main.py full_unpack archive destination\n',
-    "unpack": 'Unpacks some files of the archive to a specified destination\nUsage: python main.py unpack archive destination [file(s)]\n',
-    "help": 'Gives information about command usage\nUsage: python main.py help (command)\n',
 }
 
 if __name__ == "__main__":
